e15362678156967b89e70a77fa57c8b4b712837b
Anderson Velez <ajota06@gmail.com>
2022-02-03 23:10:27 -0500

    added close event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,15 @@ import { Component, HostListener } from "@angular/core";
 export class AppComponent {
   title = "CodeSandbox";
   openText = "close text";
+  app = "test";
 
-  @HostListener("document:sc:visor:open")
-  open() {
+  @HostListener(`document:sc:visor:open`, ["$event"])
+  open(event) {
+    console.log(event.detail);
     this.openText = "open text";
   }
 
-  @HostListener("document:sc:visor:close")
+  @HostListener("document:sc:visor:close", ["$event"])
   close() {
     this.openText = "Close text again";
   }
